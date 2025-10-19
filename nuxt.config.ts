9df5417c60b85f2d43db841c995e84e9216c15d9
Lara <laracrupni@gmail.com>
2025-10-19 19:07:03 -0300

    Suma

---
+++ nuxt.config.ts
@@ -0,0 +1,74 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase',
+    '@pinia/nuxt'
+  ],
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+        { name: 'apple-mobile-web-app-title', content: 'Suma' }
+      ],
+      link: [
+        { rel: 'apple-touch-icon', href: '/pwa-icons/apple-touch-icon.png' }
+      ]
+    }
+  },
+  supabase: {
+    redirectOptions: {
+      login: '/iniciar-sesion',
+      callback: '/callback',
+      exclude: [
+        '/iniciar-sesion',
+        '/registrarse',
+        '/olvidar-contrasena',
+        '/olvidar-contrasena-confirmacion',
+        '/restablecer-contrasena',
+      ]
+    },
+    cookieOptions: {
+      maxAge: 60 * 60 * 8,
+      secure: process.env.NODE_ENV === 'production'
+    },
+    clientOptions: {
+      auth: {
+        persistSession: true,
+        detectSessionInUrl: true,
+        autoRefreshToken: true,
+        storage: undefined
+      }
+    },
+  },
+  fonts: {
+    defaults: {
+      weights: [300, 400, 500, 600, 700, 800, 900],
+    }
+  },
+  // plugins: [
+  //   { src: '~/plugins/preload-data.js', mode: 'client' }
+  // ],
+  vite: {
+    optimizeDeps: {
+      include: ['pinia']
+    },
+    build: {
+      cssCodeSplit: true,
+      chunkSizeWarningLimit: 1000,
+    },
+    server: {
+      fs: {
+        strict: false
+      }
+    }
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+export default {
+    theme: {
+        screens: {
+            'sm': '480px',
+            'md': '660px',
+            'lg': '992px',
+            'xl': '1280px',
+            '2xl': '1440px',
+        },
+        extend: {
+            colors: {
+                primary: "#FF541C",
+                primaryHover: "#FF541C",
+                secondary: "#FF8259",
+                secondaryHover: "#FF8259",
+                terciary: "#B82D18",
+                accent: "#FCC008",
+                light: '#FBF9F9',
+                dark: "#03101D",
+                gray: {
+                    mid: "#DDDDDD"
+                },
+                error: "#DE5E5E"
+            }
+        }
+    }
+}
